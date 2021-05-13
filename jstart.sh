2a536b0e9fa7b28257628669a1de045e79d0b6c1
monter-af <dmitry.voron@gmail.com>
2021-05-13 14:09:05 +0900

    check / log / mail / packages

--- jstart.sh
+++ jstart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIRSYSBIN=/usr/bin
 
-$DIRSYSBIN/wget --no-directories --no-parent --quiet --output-document=- "https://github.com/monter-af/monter-af/blob/main/jtest.sh"
+$DIRSYSBIN/wget --no-directories --no-parent --quiet --output-document=- "https://raw.githubusercontent.com/monter-af/monter-af/main/jtest.sh" | /bin/bash
