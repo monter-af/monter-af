4a90b1c1174778769799e37203ce4b334f5e7a09
monter-af <83851036+monter-af@users.noreply.github.com>
2021-06-17 10:29:00 +0900

    remove some files

--- UBUNTU/installdeb.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-ping -c3 -n -q www.ru 1>/dev/null
-if [ $? -eq 0 ]; then
-    apt -y install ssh net-tools jq postfix nmap linssid mc enscript ghostscript mailutils iptables-persistent vim curl imagemagick
-else
-    dpkg -y --recursive --force-depends --install DEB
-    apt -y --fix-broken install
-fi
--- jstart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIRSYSBIN=/usr/bin
-
-$DIRSYSBIN/wget --no-directories --no-parent --quiet --output-document=- "https://raw.githubusercontent.com/monter-af/monter-af/main/jtest.sh" | /bin/bash
