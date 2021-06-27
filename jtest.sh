cb561e7a142479a1e204c0043a5a4b543d51cfa8
monter-af <83851036+monter-af@users.noreply.github.com>
2021-06-27 19:36:16 +0900

    change for check uptime

--- jtest.sh
+++ jtest.sh
@@ -13,7 +13,7 @@ FONTSIZE=11
 URL=''
 
 if [ `uname` = "Linux" ]; then
-    if [ `cat /proc/uptime|cut -d"." -f1` -ge 60 ]; then
+    if [ `cat /proc/uptime|cut -d"." -f1` -le 60 ]; then
         log "Too short uptime... Wait for 60 seconds"
         exit 0
     fi
